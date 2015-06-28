6b57bf1a6e4f2ff5efe1d6fc44a9b9e6b49daeb9
Alex Brims <alex.brims@gmail.com>
2015-06-28 22:16:36 +0100

    Setup for unit testing

---
+++ Tests/Component/ColorTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Outspaced\GoogleChartMakerBundle\Tests\Component;
+
+class ColorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTheTesting()
+    {
+        $this->assertTrue(TRUE);
+    }
+}
---
+++ Tests/bootstrap.php
@@ -0,0 +1,5 @@
+<?php 
+
+// @todo fix this silliness
+$loader = require_once __DIR__.'/../../../../../../vendor/autoload.php';
+$loader->add('Outspaced\\GoogleChartMaker\\Tests', __DIR__);
