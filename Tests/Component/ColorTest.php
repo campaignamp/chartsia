ab64dc255f7f8990e2d30a2b0a8912d529632be1
Alex Brims <alex.brims@gmail.com>
2015-06-29 20:42:37 +0100

    Actual tests for the Color class

--- Tests/Component/ColorTest.php
+++ Tests/Component/ColorTest.php
@@ -2,10 +2,23 @@
 
 namespace Outspaced\GoogleChartMakerBundle\Tests\Component;
 
+use \Outspaced\GoogleChartMakerBundle\Chart\Component\Color;
+
 class ColorTest extends \PHPUnit_Framework_TestCase
 {
-    public function testTheTesting()
+    public function testConstructWithColor()
     {
-        $this->assertTrue(TRUE);
+        $color = new Color('FF0000');
+        
+        $this->assertEquals($color->getColor(), 'FF0000');
     }
+
+    public function testSetColor()
+    {
+        $color = new Color();
+        $color->setColor('FF0000');
+        
+        $this->assertEquals($color->getColor(), 'FF0000');
+    }
+    
 }
