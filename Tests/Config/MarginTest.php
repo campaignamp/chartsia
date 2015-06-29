400d41995845db17a7a073859e1c4e53ffb78d04
Alex Brims <alex.brims@gmail.com>
2015-06-29 22:14:57 +0100

    Implemented Margin test

---
+++ Tests/Config/MarginTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Outspaced\GoogleChartMakerBundle\Tests\Config;
+
+use \Outspaced\GoogleChartMakerBundle\Chart\Config\Margin;
+
+class MarginTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstructWithMargin()
+    {
+        $margin = new Margin(10, 20, 30, 40);
+        $this->assertEquals($margin->getLeft(), 10);
+        $this->assertEquals($margin->getRight(), 20);
+        $this->assertEquals($margin->getTop(), 30);
+        $this->assertEquals($margin->getBottom(), 40);
+    }
+
+    public function testSetValues()
+    {
+        $margin = new Margin(10, 20, 30, 40);
+
+        $margin->getLeft(10);
+        $margin->getRight(20);
+        $margin->getTop(30);
+        $margin->getBottom(40);
+
+        $this->assertEquals($margin->getLeft(), 10);
+        $this->assertEquals($margin->getRight(), 20);
+        $this->assertEquals($margin->getTop(), 30);
+        $this->assertEquals($margin->getBottom(), 40);
+    }
+}
