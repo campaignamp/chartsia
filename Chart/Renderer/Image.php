e3a0b882b95e6d608ff6c686f16094ca9369f986
Alex Brims <alex.brims@gmail.com>
2015-06-29 21:59:15 +0100

    Removed excess spaces

--- Chart/Renderer/Image.php
+++ Chart/Renderer/Image.php
@@ -13,13 +13,13 @@ class Image
 	 * @var string
 	 */
 	const BASE_URL = 'http://chart.googleapis.com/chart?';
-    
+
     public function render(BaseChart $chart)
     {
         $url = self::BASE_URL;
-        
+
         // Now it needs to run through the data returned by getData() and form it into a URL
-        
+
         // TYPE
         $url .= 'cht='. $chart->getType() .'&';
 
@@ -29,12 +29,12 @@ class Image
         // TITLE
         if ($title = $chart->getTitle()) {
             $url .= 'chtt='. urlencode($title->getTitle()) .'&';
-            
+
             if ($title->getColor()) {
                 $url .= 'chts='. $title->getColor()->getColor() .'&';
             }
         }
-            
+
         // SOME ELEMENT
         foreach ($chart->getElements() as $chartElement) {
             $url .= $chartElement->getKey() . '=' . $chartElement->render() .'&';
@@ -45,23 +45,23 @@ class Image
         // Start with a loop then refactor
         $data = [];
         $lineColors = [];
-        
+
         foreach ($chart->getDataSets() as $dataSet) {
 
             $data[] = implode(',', $dataSet->getData());
-            
+
             // OK so how to handle the chart-specific elements?
             if ($dataSet->getColor()) {
                 $lineColors[] = $dataSet->getColor()->getColor();
             }
         }
-        
+
         $url .= 'chd=t:'. implode('|', $data) .'&';
 
         if ($lineColors) {
             $url .= 'chco='. implode(',', $lineColors) .'&';
         }
-        
+
         return $url;
     }
 }
