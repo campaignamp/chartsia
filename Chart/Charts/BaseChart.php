fd8ecd39333db9c694270058c4f5272a09e915e8
Alex Brims <alex.brims@gmail.com>
2015-07-08 21:48:02 +0100

    Started to implement Axis in chart

--- Chart/Charts/BaseChart.php
+++ Chart/Charts/BaseChart.php
@@ -44,6 +44,26 @@ abstract class BaseChart
     protected $legend;
 
     /**
+     * @var Axis\Axis
+     */
+    protected $topAxis;
+
+    /**
+     * @var Axis\Axis
+     */
+    protected $bottomAxis;
+
+    /**
+     * @var Axis\Axis
+     */
+    protected $leftAxis;
+
+    /**
+     * @var Axis\Axis
+     */
+    protected $rightAxis;
+
+    /**
      * @param  Element\ElementInterface $element
      * @return self
      */
@@ -144,6 +164,83 @@ abstract class BaseChart
     {
         return $this->legend;
     }
+
+    /**
+     * @param  Axis\Axis  $axis
+     * @return self
+     */
+    public function setTopAxis(Axis\Axis $axis)
+    {
+        $this->topAxis = $axis;
+
+        return $this;
+    }
+
+    /**
+     * @return Axis
+     */
+    public function getTopAxis()
+    {
+        return $this->topAxis;
+    }
+
+    /**
+     * @param  Axis\Axis  $axis
+     * @return self
+     */
+    public function setBottomAxis(Axis\Axis $axis)
+    {
+        $this->bottomAxis = $axis;
+
+        return $this;
+    }
+
+    /**
+     * @return Axis
+     */
+    public function getBottomAxis()
+    {
+        return $this->bottomAxis;
+    }
+
+    /**
+     * @param  Axis\Axis  $axis
+     * @return self
+     */
+    public function setLeftAxis(Axis\Axis $axis)
+    {
+        $this->leftAxis = $axis;
+
+        return $this;
+    }
+
+    /**
+     * @return Axis
+     */
+    public function getLeftAxis()
+    {
+        return $this->leftAxis;
+    }
+
+    /**
+     * @param  Axis\Axis  $axis
+     * @return self
+     */
+    public function setRightAxis(Axis\Axis $axis)
+    {
+        $this->rightAxis = $axis;
+
+        return $this;
+    }
+
+    /**
+     * @return Axis
+     */
+    public function getRightAxis()
+    {
+        return $this->rightAxis;
+    }
+
 
     /**
      * @return string
